b6bfc9b740c5fbf85d4248ae0a5e1034cce94624
Joonas Lindroos <joonas.lindroos@outlook.com>
2026-03-08 18:03:13 +0200

    fixed favicon not displaying

--- app/layout.js
+++ app/layout.js
@@ -32,7 +32,7 @@ export default function RootLayout({ children }) {
     <html className="dark overscroll-none" lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/res/images/favicon.ico" />
+        <link rel="icon" href="/images/favicon.ico" />
         <link rel="preload" href="/fonts/New_Amsterdam/NewAmsterdam-Regular.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
       </head>
       <body className="bg-neutral-950 text-white overscroll-none overflow-x-hidden">
